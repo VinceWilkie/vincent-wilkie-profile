1c344eefdac7de448d3a2f17f9b812e870a8805f
Vincent Wilkie <anonymousvince@gmail.com>
2025-01-19 03:22:54 -0500

    Updates!

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,29 @@
 "use client";
+import Image from "next/image";
+import { LinkedinIcon, LinkedinShareButton } from "next-share";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <h1 className="text-2xl">Vincent Wilkie</h1>
+    <div className="container mx-auto flex flex-col items-center justify-center">
+      <Image
+        src="/logo.png"
+        width={300}
+        height={300}
+        alt="VW Logo"
+        priority
+      />
+      <h1 className="text-2xl mb-4">Welcome!</h1>
+      <Link href="https://www.linkedin.com/in/vdubworkshop/" target="_blank" className="pushable">
+        <span className="pushable-shadow"></span>
+        <span className="edge"></span>
+        <span className="front">
+          <span className="flex items-center gap-2">
+            <LinkedinIcon size={32} className="rounded" />
+            LinkedIn
+          </span>
+        </span>
+      </Link>
+    </div>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,13 +2,16 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex justify-center items-center h-full">
-      <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center justify-center w-full gap-4">
+      <div>
         <h1 className="text-6xl">404</h1>
         <h2 className="text-2xl">Not Found</h2>
-        <p>Could not find requested resource</p>
-        <Link href="/">Return Home</Link>
       </div>
+      <Link href="/" className="pushable">
+        <span className="pushable-shadow"></span>
+        <span className="edge"></span>
+        <span className="front">Return Home</span>
+      </Link>
     </div>
   );
 }
