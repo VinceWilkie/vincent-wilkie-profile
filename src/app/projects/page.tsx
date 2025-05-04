26a52d0a26c4e050986cb3988a1e40b9f2cb8f53
Vincent Wilkie <anonymousvince@gmail.com>
2025-05-04 10:14:13 -0400

    New projects component + updating projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import VWLink from "@/components/VWLink";
-import Project from "@/models/Project";
+import ProjectModel from "@/models/Project";
+import ProjectComponent from "@/components/Project";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect } from "react";
@@ -12,7 +12,7 @@ export default function Page() {
     // }, 1000);
   }, []);
 
-  const ctrlAltRandom = new Project();
+  const ctrlAltRandom = new ProjectModel();
   ctrlAltRandom.parseJson({
     name: "The Ctrl Alt Random Podcast",
     description: "Welcome to the Ctrl + Alt + Random podcast, where we dive into random tech topics—from groundbreaking innovations to quirky gadgets. Expect fun, candid chats and surprising discoveries in every episode. Tech has never been this unpredictable—hit play and join the randomness!",
@@ -44,31 +44,9 @@ export default function Page() {
         <h1 className="text-3xl text-center">Projects</h1>
         <p className="text-xl text-center"></p>
       </div>
-      <div className="project-list flex flex-col gap-4 mb-14 overflow-y-scrollw">
-        {projects.map(({ name, description, image: { src, alt, width, height }, url, urlText, urlClasses }, index) => (
-          <div
-            key={`project-list-item-`+index}
-            className={`project-list-item p-4 shadow rounded-xl border flex flex-col md:flex-row items-center md:items-start text-center md:text-left gap-4 bg-gray-100`}
-          >
-            <Image
-              className="visible rounded-xl"
-              src={src}
-              alt={alt}
-              width={width}
-              height={height}
-            />
-            <div className="info flex flex-col gap-4">
-              <h1 className="text-xl font-bold">{name}</h1>
-              <h1 className="text-lg text-left">About:</h1>
-              <p className="text-justify">{description}</p>
-              <VWLink href={url} classes={`w-full${urlClasses ? ` ` + urlClasses : ``}`}>
-                <div className="flex justify-between w-full font-bold">
-                  <span>{urlText}</span>
-                  <span>&rarr;</span>
-                </div>
-              </VWLink>
-            </div>
-          </div>
+      <div className="project-list flex flex-col gap-4 mb-14 overflow-y-scroll">
+        {projects.map((project, index) => (
+          <ProjectComponent project={project} key={`project-list-item-`+index} />
         ))}
         <div className="flex flex-col items-center p-4">
           <h1 className="text-3xl font-bold">More Coming Soon</h1>
---
+++ src/components/Project.tsx
@@ -0,0 +1,35 @@
+"use client";
+import VWLink from "@/components/VWLink";
+import ProjectModel from "@/models/Project";
+import Image from "next/image";
+
+export default function Project({
+  project
+}: Readonly<{
+  project: ProjectModel
+}>) {
+  return (
+    <div className={`project-list-item p-4 shadow rounded-xl border flex flex-col md:flex-row items-center md:items-start text-center md:text-left gap-4 bg-gray-100`}>
+      <Image
+        className="visible rounded-xl"
+        src={project.image.src}
+        alt={project.image.alt}
+        width={project.image.width}
+        height={project.image.height}
+      />
+      <div className="info flex flex-col h-full justify-between">
+        <div className="about">
+          <h1 className="text-xl font-bold mb-2">{project.name}</h1>
+          <h1 className="text-lg text-left mb-1">About:</h1>
+          <p className="text-justify mb-4">{project.description}</p>
+        </div>
+        <VWLink href={project.url} classes={`w-full${project.urlClasses ? ` ` + project.urlClasses : ``}`}>
+          <div className="flex justify-between w-full font-bold">
+            <span>{project.urlText}</span>
+            <span>&rarr;</span>
+          </div>
+        </VWLink>
+      </div>
+    </div>
+  );
+}
