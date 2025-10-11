e6abc216dbeb22e7746a6ab6f4d11588cb57f6e4
Vincent Wilkie <anonymousvince@gmail.com>
2025-10-11 15:08:54 -0400

    mobile viewport fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ const robotoMono = Roboto_Mono({
 export const metadata: Metadata = {
   title: "Wilkie's Workshop",
   description: "Created by Vincent Wilkie",
+  viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({
